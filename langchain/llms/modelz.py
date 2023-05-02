3762b4a601de9ad4c00ddb0784365752155a5e1a
Ce Gao <cegao@tensorchord.ai>
2023-05-02 12:09:19 +0800

    rebase
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- langchain/llms/modelz.py
+++ langchain/llms/modelz.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, List, Mapping, Optional
 
 from pydantic import Extra, Field, root_validator
 
+from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.llms.base import LLM
 from langchain.llms.utils import enforce_stop_tokens
 from langchain.utils import get_from_dict_or_env
@@ -78,7 +79,12 @@ class Modelz(LLM):
         """Return type of model."""
         return "modelz"
 
-    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+    ) -> str:
         """Call to replicate endpoint."""
         try:
             import modelz
